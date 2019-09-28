645397548ad61fd705a772c3035007d93a7518a7
NamHyunJun <sos4100@kaist.ac.kr>
2019-09-28 13:31:11 +0900

    [Priority] T_get_nice, T_set_nice, T_load_avg, T_rec_cpu

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -94,6 +94,9 @@ struct thread
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
+	int niceV;							// -20<= nice <= 20 initial value = 0
+	float loadAvg;						// initial value = 0
+	float recCpu;						// initial value = 0
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
