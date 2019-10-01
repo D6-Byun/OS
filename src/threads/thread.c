9a90055c632a065fa4f758e18fc3f4b67f5f6c75
DESKTOP-79LIHM0\D6 <bighc0107@naver.com>
2019-10-01 20:12:09 +0900

    send to linux

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -72,7 +72,7 @@ static void *alloc_frame (struct thread *, size_t size);
 static void schedule (void);
 void thread_schedule_tail (struct thread *prev);
 static tid_t allocate_tid (void);
-static void timer_awake_thread(int64_t ticks)
+static void timer_awake_thread(int64_t ticks);
 
 /* Initializes the threading system by transforming the code
    that's currently running into a thread.  This can't work in
--- src/threads/thread.h
+++ src/threads/thread.h
@@ -130,8 +130,6 @@ const char *thread_name (void);
 void thread_exit (void) NO_RETURN;
 void thread_yield (void);
 
-bool is_thread (struct thread *t);
-
 /* Performs some operation on thread t, given auxiliary data AUX. */
 typedef void thread_action_func (struct thread *t, void *aux);
 void thread_foreach (thread_action_func *, void *);
