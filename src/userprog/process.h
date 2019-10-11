c55fe951697b1cb0960f3f7f8718428152892fc1
DESKTOP-79LIHM0\D6 <bighc0107@naver.com>
2019-10-12 00:23:36 +0900

    God knows

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -3,9 +3,16 @@
 
 #include "threads/thread.h"
 
-tid_t process_execute (const char *file_name);
-int process_wait (tid_t);
-void process_exit (void);
-void process_activate (void);
+struct arg
+{
+	char * argv[32];
+	int argc;
+	char * filename;
+};
+
+tid_t process_execute(const char *file_name);
+int process_wait(tid_t);
+void process_exit(void);
+void process_activate(void);
 
 #endif /* userprog/process.h */
