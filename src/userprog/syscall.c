ece378e1fd07763f40834815cef7c57378759535
DESKTOP-79LIHM0\D6 <bighc0107@naver.com>
2019-10-12 16:23:45 +0900

    syscall structure added

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,7 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -12,9 +13,68 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+
+
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+	switch (*(uintptr_t *)(f.esp - 4)) 
+	{
+	case SYS_HALT: /* arg 0 */
+	{
+		shutdown_power_off();
+		break;
+	}
+	case SYS_EXIT: /* arg 1 */
+	{
+		break;
+	}
+	case SYS_EXEC: /* arg 1 */
+	{
+		
+		break;
+	}
+	case SYS_WAIT: /* arg 1 */
+	{
+		break;
+	}
+	case SYS_CREATE: /* arg 2 */
+	{
+		break;
+	}
+	case SYS_REMOVE: /* arg 1 */
+	{
+		break;
+	}
+	case SYS_OPEN: /* arg 1 */
+	{
+		break;
+	}
+	case SYS_FILESIZE: /* arg 1 */
+	{
+		break;
+	}
+	case SYS_READ: /* arg 3 */
+	{
+		break;
+	}
+	case SYS_WRITE: /* arg 3 */
+	{
+		break;
+	}
+	case SYS_SEEK: /* arg 2 */
+	{
+		break;
+	}
+	case SYS_TELL: /* arg 1 */
+	{
+		break;
+	}
+	case SYS_CLOSE: /* arg 1 */
+	{
+		break;
+	}
+	}
   printf ("system call!\n");
   thread_exit ();
 }
