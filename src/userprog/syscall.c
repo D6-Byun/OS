0d2d0668425251f2983cb27083eb2d0a0ab56044
DESKTOP-79LIHM0\D6 <bighc0107@naver.com>
2019-10-15 16:34:08 +0900

    syscall debug 1

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,6 +1,7 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include <stdint.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "devices/shutdown.h"
@@ -10,6 +11,8 @@
 #include "filesys/file.h"
 
 static void syscall_handler (struct intr_frame *);
+static void arg_catcher(uintptr_t* args[], int num, void *esp);
+static void is_pointer_valid(uintptr_t* ptr);
 
 void
 syscall_init (void) 
@@ -22,7 +25,7 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
 	uintptr_t *args[4];
 
-	switch (*(uintptr_t *)(f.esp - 4)) 
+	switch (*(uintptr_t *)(f->esp))
 	{
 	case SYS_HALT: /* arg 0 */
 	{
@@ -31,14 +34,14 @@ syscall_handler (struct intr_frame *f UNUSED)
 	}
 	case SYS_EXIT: /* arg 1 */
 	{
-		arg_catcher(args[4], 2);
+		arg_catcher(args[4], 2, f->esp);
 		thread_exit();
 		break;
 	}
 	case SYS_EXEC: /* arg 1 */
 	{
 		int pid;
-		arg_catcher(args[4], 2);
+		arg_catcher(args[4], 2, f->esp);
 		pid = process_execute(*args[1]);
 		f->eax = pid;
 		break;
@@ -46,7 +49,7 @@ syscall_handler (struct intr_frame *f UNUSED)
 	case SYS_WAIT: /* arg 1 */
 	{
 		int child_pid;
-		arg_catcher(args[4], 2);
+		arg_catcher(args[4], 2, f->esp);
 		child_pid = *args[1];
 		process_wait(child_pid);
 		f->eax = 0; //should be implemented here
@@ -54,8 +57,8 @@ syscall_handler (struct intr_frame *f UNUSED)
 	}
 	case SYS_CREATE: /* arg 2 */
 	{
-		arg_catcher(args[4], 3);
-		if (!filesys_create(*args[1], *arg[2]))
+		arg_catcher(args[4], 3, f->esp);
+		if (!filesys_create(*args[1], *args[2]))
 		{
 			f->eax = 0;
 		}
@@ -64,7 +67,7 @@ syscall_handler (struct intr_frame *f UNUSED)
 	}
 	case SYS_REMOVE: /* arg 1 */
 	{
-		arg_catcher(args[4], 2);
+		arg_catcher(args[4], 2, f->esp);
 		if (!filesys_remove(*args[1]))
 		{
 			f->eax = 0;
@@ -75,7 +78,7 @@ syscall_handler (struct intr_frame *f UNUSED)
 	case SYS_OPEN: /* arg 1 */
 	{
 		struct thread * cur = thread_current();
-		arg_catcher(args[4], 2);
+		arg_catcher(args[4], 2, f->esp);
 		cur->fd_table[cur->fd_num] = filesys_open(*args[1]);
 		f->eax = cur->fd_num;
 		cur->fd_num += 1;
@@ -85,7 +88,7 @@ syscall_handler (struct intr_frame *f UNUSED)
 	{
 		struct thread * cur = thread_current();
 		struct file * target_file;
-		arg_catcher(args[4], 2);
+		arg_catcher(args[4], 2, f->esp);
 		target_file = cur->fd_table[*args[1]];
 		f->eax = file_length(target_file);
 		break;
@@ -94,34 +97,34 @@ syscall_handler (struct intr_frame *f UNUSED)
 	{
 		struct thread * cur = thread_current();
 		struct file * target_file;
-		arg_catcher(args[4], 4);
+		arg_catcher(args[4], 4, f->esp);
 		target_file = cur->fd_table[*args[1]];
-		f->eax = file_read(target_file, *arg[2], *arg[3]);
+		f->eax = file_read(target_file, *args[2], *args[3]);
 		break;
 	}
 	case SYS_WRITE: /* arg 3 */
 	{
 		struct thread * cur = thread_current();
 		struct file * target_file;
-		arg_catcher(args[4], 4);
+		arg_catcher(args[4], 4, f->esp);
 		target_file = cur->fd_table[*args[1]];
-		f->eax = file_write(target_file, *arg[2], *arg[3]);
+		f->eax = file_write(target_file, *args[2], *args[3]);
 		break;
 	}
 	case SYS_SEEK: /* arg 2 */
 	{
 		struct thread * cur = thread_current();
 		struct file * target_file;
-		arg_catcher(args[4], 3);
+		arg_catcher(args[4], 3, f->esp);
 		target_file = cur->fd_table[*args[1]];
-		file_seek(target_file, *arg[2]);
+		file_seek(target_file, *args[2]);
 		break;
 	}
 	case SYS_TELL: /* arg 1 */
 	{
 		struct thread * cur = thread_current();
 		struct file * target_file;
-		arg_catcher(args[4], 2);
+		arg_catcher(args[4], 2, f->esp);
 		target_file = cur->fd_table[*args[1]];
 		f->eax = file_tell(target_file);
 		break;
@@ -130,7 +133,7 @@ syscall_handler (struct intr_frame *f UNUSED)
 	{
 		struct thread * cur = thread_current();
 		struct file * target_file;
-		arg_catcher(args[4], 2);
+		arg_catcher(args[4], 2, f->esp);
 		target_file = cur->fd_table[*args[1]];
 		file_close(target_file);
 		break;
@@ -139,16 +142,16 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
 }
 
-void arg_catcher(uintptr_t* args[], int num, void *esp)
+static void arg_catcher(uintptr_t* args[], int num, void *esp)
 {
 	for (int i = 0; i < num; i++)
 	{
 		is_pointer_valid((uintptr_t*)(esp + 4 * i));
-		arg[i] = (uintptr_t*)(esp + 4 * i));
+		args[i] = (uintptr_t*)(esp + 4 * i);
 	}
 }
 
-void is_pointer_valid(uintptr_t* ptr)
+static void is_pointer_valid(uintptr_t* ptr)
 {
 	int casted_ptr = (int)ptr;
 	if (casted_ptr < 0x08048000 || is_kernel_vaddr(ptr))
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -2,7 +2,5 @@
 #define USERPROG_SYSCALL_H
 
 void syscall_init (void);
-void arg_catcher(uintptr_t* args[], int num, void *esp);
-void is_pointer_valid(uintptr_t* ptr);
 
 #endif /* userprog/syscall.h */
