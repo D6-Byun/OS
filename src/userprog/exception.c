4e5aff162ba1783c5885f823696c58fb24b631ac
DESKTOP-79LIHM0\D6 <bighc0107@naver.com>
2019-10-16 17:40:40 +0900

    exception modified

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -4,6 +4,7 @@
 #include "userprog/gdt.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
@@ -143,10 +144,18 @@ page_fault (struct intr_frame *f)
   /* Count page faults. */
   page_fault_cnt++;
 
+  if (not_present)
+
   /* Determine cause. */
   not_present = (f->error_code & PF_P) == 0;
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
+
+  if (!user || is_kernel_vaddr(fault_addr)
+  {
+	  exit(-1);
+  }
+  if ()
 
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
