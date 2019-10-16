34f85553a18d9ecff4073f4f1fb81b715efcb00e
DESKTOP-79LIHM0\D6 <bighc0107@naver.com>
2019-10-16 14:49:05 +0900

    d -> x

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -211,7 +211,7 @@ static void arg_catcher(uint32_t* args[], int num, void *esp)
 static void is_pointer_valid(uint32_t* ptr)
 {
 	unsigned int casted_ptr = (unsigned int)ptr;
-	printf("pointer invalid? : %d", casted_ptr);
+	printf("pointer invalid? : %x", casted_ptr);
 	if (casted_ptr < 0x08048000 || is_kernel_vaddr(ptr))
 	{
 		//printf("invalid pointer %d", casted_ptr);
