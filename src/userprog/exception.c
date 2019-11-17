3cf41ab116ecafbc6a19bd27a61b67d8f2bdeccc
NamHyunJun <sos4100@kaist.ac.kr>
2019-11-17 16:12:07 +0900

    more fixed

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -166,12 +166,14 @@ page_fault (struct intr_frame *f)
 		  PANIC("SEGMENTATION FAULT");
 	  }
   }
-  
-  printf("Page fault at %p: %s error %s page in %s context.\n",
-	  fault_addr,
-	  not_present ? "not present" : "rights violation",
-	  write ? "writing" : "reading",
-	  user ? "user" : "kernel");
-  kill(f);
+  else {
+	  printf("Page fault at %p: %s error %s page in %s context.\n",
+		  fault_addr,
+		  not_present ? "not present" : "rights violation",
+		  write ? "writing" : "reading",
+		  user ? "user" : "kernel");
+	  kill(f);
+  }
+
 }
 
