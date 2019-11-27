a35d5857784f9eaa87984e5bd10af0291423bd70
DESKTOP-79LIHM0\D6 <bighc0107@naver.com>
2019-11-27 22:07:15 +0900

    change format of init.c. problem was about not adding frame.h in init.c

--- src/threads/init.c
+++ src/threads/init.c
@@ -31,6 +31,9 @@
 #else
 #include "tests/threads/tests.h"
 #endif
+#ifdef VM
+#include "vm/frame.h"
+#endif 
 #ifdef FILESYS
 #include "devices/block.h"
 #include "devices/ide.h"
@@ -127,7 +130,9 @@ main (void)
   filesys_init (format_filesys);
 #endif
 
+#ifdef VM
   frame_init();
+#endif
 
   printf ("Boot complete.\n");
 
