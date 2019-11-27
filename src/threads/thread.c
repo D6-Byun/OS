e8393b68461319b0f1c2113d78e92c26ee531d44
DESKTOP-79LIHM0\D6 <bighc0107@naver.com>
2019-11-27 21:51:01 +0900

    change position of frame init because of current_thread problem

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -94,13 +94,13 @@ thread_init (void)
   list_init (&ready_list);
   list_init (&all_list);
 
-  frame_init();
-
   /* Set up a thread structure for the running thread. */
   initial_thread = running_thread ();
   init_thread (initial_thread, "main", PRI_DEFAULT);
   initial_thread->status = THREAD_RUNNING;
   initial_thread->tid = allocate_tid ();
+
+  frame_init();
 }
 
 /* Starts preemptive thread scheduling by enabling interrupts.
