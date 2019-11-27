fc1311c4a998739abc0a54f81b74f8f4c1d1350a
NamHyunJun <sos4100@kaist.ac.kr>
2019-11-27 23:14:52 +0900

    plz

--- src/vm/page.h
+++ src/vm/page.h
@@ -27,15 +27,13 @@ struct sup_page_entry{
 	enum status status;
 };
 
-
 struct sup_page_table *spt_create(void);
 void spt_destroy(struct sup_page_table *spt);
 struct sup_page_entry *spt_lookup(struct sup_page_table *spt, void *upage);
-bool sup_load_file(struct sup_page_entry *spte);
-bool sup_load_mmap(struct sup_page_entry *spte);
+bool spt_load_file(struct sup_page_entry *spte);
+bool spt_load_mmap(struct sup_page_entry *spte);
 bool spt_load_swap(struct sup_page_entry *spte);
 bool spt_load_page(struct sup_page_table *spt, void *upage);
-bool sup_add_entry(struct sup_page_table *spt, struct file *file, off_t ofs, void *upage,uint32_t read_bytes, uint32_t zero_bytes,bool writable);
-
+bool sup_add_entry(struct sup_page_table *spt, struct file *file, off_t ofs, void *upage, uint32_t read_bytes, uint32_t zero_bytes, bool writable);
 
 #endif
