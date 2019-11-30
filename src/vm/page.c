88657e990d37a690eeccce293f2f6b786b8aa6ff
DESKTOP-79LIHM0\D6 <bighc0107@naver.com>
2019-11-30 22:12:13 +0900

    debug code added

--- src/vm/page.c
+++ src/vm/page.c
@@ -110,14 +110,14 @@ void spt_destroy(struct spt *spt)
 
 static void spt_entry_destroy(struct hash_elem *e, void *aux)
 {
-	//printf("start spt_entry_destroy\n");
+	printf("start spt_entry_destroy\n");
 	struct spt_entry * target_entry = hash_entry(e, struct spt_entry, helem);
 	//printf("right target_entry\n");
 	if (target_entry->is_loaded)
 	{
 		//printf("need to change format of frame entry deletion\n");
 		//printf("connected with frame\n");
-		//printf("kpage addr : %x\n", target_entry->kpage);
+		printf("kpage addr : %x\n", target_entry->kpage);
 
 		struct frame_entry *target_frame = search_frame_entry(target_entry->kpage);
 
