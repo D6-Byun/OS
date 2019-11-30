1870dff907306887ed680173dbfc2397de1d2a07
DESKTOP-79LIHM0\D6 <bighc0107@naver.com>
2019-11-30 17:50:09 +0900

    not a problem in exception. debug code added in munmap

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -443,6 +443,7 @@ void munmap(mapid_t mapid)
 {
 	struct mmap_file * target_mmap = NULL;
 	struct list_elem *e;
+	printf("munmap start\n");
 	if (!list_empty(&thread_current()->mmap_list))
 	{
 		for (e = list_begin(&thread_current()->mmap_list); e != list_end(&thread_current()->mmap_list); e = list_next(e))
@@ -456,14 +457,14 @@ void munmap(mapid_t mapid)
 		}
 		if (target_mmap == NULL)
 		{
-			//printf("target_mmap is null in munmap\n");
+			printf("target_mmap is null in munmap\n");
 			exit(-1);
 		}
 		free_mmap(target_mmap);
 	}
 	else
 	{
-		//printf("mmap_list is empty in munmap\n");
+		printf("mmap_list is empty in munmap\n");
 		exit(-1);
 	}
 }
